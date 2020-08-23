188c0270ab4aff063201e2fd4e70f57c6d5b6d0b
SerjLeoDev <sergejleontev111@gmail.com>
2020-08-23 16:56:49 +0300

    Core setup

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+
+const plugins = () => ([
+    new MiniCssExtractPlugin(
+        {
+            filename: 'styles.css'
+        }
+    ),
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new CleanWebpackPlugin()
+])
+
+module.exports = {
+    entry: './src/index.ts',
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: plugins(),
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude:/node_modules/
+            }
+        ],
+   },
+};
